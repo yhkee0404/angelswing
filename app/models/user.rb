ec6162b75e1e6ec349336179c0aa75c72543307d
yhkee0404 <yhkee0404@gmail.com>
2022-10-26 10:05:50 +0900

    Add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,23 @@
 class User < ApplicationRecord
+  acts_as_authentic
+
+  # Validate email, login, and password as you see fit.
+  #
+  # Authlogic < 5 added these validation for you, making them a little awkward
+  # to change. In 4.4.0, those automatic validations were deprecated. See
+  # https://github.com/binarylogic/authlogic/blob/master/doc/use_normal_rails_validation.md
+  validates :email,
+    format: {
+      with: /@/,
+      message: "should look like an email address."
+    },
+    length: { maximum: 100 },
+    uniqueness: {
+      case_sensitive: false
+    }
+
+  validates :password,
+    length: {
+      minimum: 8,
+    }
 end
