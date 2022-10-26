faf3bc0b1eaace2b80220219b2942d9664262042
yhkee0404 <yhkee0404@gmail.com>
2022-10-26 19:02:36 +0900

    Migrate to rename user name columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_25_224537) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_26_094244) do
   create_table "users", force: :cascade do |t|
-    t.string "firstname"
-    t.string "lastname"
+    t.string "first_name"
+    t.string "last_name"
     t.string "country"
     t.string "email"
     t.string "crypted_password"
