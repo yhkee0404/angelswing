7ae84aa4c2cf9daac7e10b5e5312aeb1c16d5db4
yhkee0404 <yhkee0404@gmail.com>
2022-10-30 05:12:47 +0900

    Migrate contents

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_27_144253) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_28_094934) do
+  create_table "contents", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_contents_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -22,4 +31,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_144253) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "contents", "users"
 end
