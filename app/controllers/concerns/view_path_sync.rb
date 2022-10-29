0f3a6fd4ecfb604d26b791c0881bee24ae6fdc90
yhkee0404 <yhkee0404@gmail.com>
2022-10-29 21:31:37 +0900

    Update ViewPathSync with expression substitution

--- app/controllers/concerns/view_path_sync.rb
+++ app/controllers/concerns/view_path_sync.rb
@@ -2,6 +2,6 @@ module ViewPathSync
   extend ActiveSupport::Concern
 
   included do
-    prepend_view_path view_paths[0].to_s + '/' + module_parents[-3].to_s.underscore
+    prepend_view_path "#{view_paths[0].to_s}/#{module_parents[-3].to_s.underscore}"
   end
 end
