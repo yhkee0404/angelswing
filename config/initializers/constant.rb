31cb28d67a6db32f54e466ae21e5164d6bea0869
yhkee0404 <yhkee0404@gmail.com>
2022-10-29 21:56:59 +0900

    Update DATETIME_FORMAT_STRING with colon offset

--- config/initializers/constant.rb
+++ config/initializers/constant.rb
@@ -1,1 +1,2 @@
-DATETIME_FORMAT_STRING = "%Y-%m-%dT%H:%M%z"
+# https://stackoverflow.com/questions/37312511/how-do-you-get-the-local-timezone-offset-in-hhmm-format
+DATETIME_FORMAT_STRING = "%Y-%m-%dT%H:%M%:z"
